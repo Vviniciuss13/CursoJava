4a067a3f36e9c39ba7d4f374f1c5efcce076732e
Vviniciuss13 <viniciusfravolini@gmail.com>
2025-12-06 12:31:06 -0300

    getters and setters

--- src/oo/encapsulamento/Pessoa.java
+++ src/oo/encapsulamento/Pessoa.java
@@ -2,10 +2,12 @@ package oo.encapsulamento;
 
 public class Pessoa {
     private String nome;
+    private String sobrenome;
     private int idade;
 
-    public Pessoa(String nome, int idade){
+    public Pessoa(String nome, String sobrenome, int idade){
         setNome(nome);
+        setSobrenome(sobrenome);
         setIdade(idade);
     }
 
@@ -15,6 +17,18 @@ public class Pessoa {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNomeCompleto(){
+        return getNome() + " " + getSobrenome();
     }
 
     // Getter
